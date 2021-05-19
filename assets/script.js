be2a695cf03fdc2c3a7a17d0175cb2a1bc076619
Ana Ea <aabad0464@gmail.com>
2021-05-18 18:53:59 -0600

    added on click function

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,11 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
     //assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
-        //get nearby values.
-//         console.log(this);
-//         var text = $(this).siblings(".description").val(); 
-//         var time = $(this).parent().attr("id"); 
+        console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
 
-//         //Save to Local Storage
-//         localStorage.setItem(time, text);
-//     })
-// })
+        //Save to Local Storage
+        // localStorage.setItem(time, text);
+    })
+})
