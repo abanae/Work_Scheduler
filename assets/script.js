f888260bd74c1a660f2f0c81a74dc9eed25d6654
Ana Ea <aabad0464@gmail.com>
2021-05-18 18:50:37 -0600

    added function ready

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+
+$(document).ready(function(){ 
+    //Displays current day & time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
+    //assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+//         console.log(this);
+//         var text = $(this).siblings(".description").val(); 
+//         var time = $(this).parent().attr("id"); 
+
+//         //Save to Local Storage
+//         localStorage.setItem(time, text);
+//     })
+// })
