01903d37ffaff5678a800322a976ce0511f4afef
Ana Ea <aabad0464@gmail.com>
2021-05-20 21:52:55 -0600

    saving data to local storage

--- assets/script.js
+++ assets/script.js
@@ -15,15 +15,16 @@ $().ready(function(){
         localStorage.setItem(time, value);
     })
 
-    // $("#hr9 .description").val(localStorage.getItem("hr9"));
-    // $("#hr10 .description").val(localStorage.getItem("hr10"));
-    // $("#hr11 .description").val(localStorage.getItem("hr11"));
-    // $("#hr12 .description").val(localStorage.getItem("hr12"));
-    // $("#hr13 .description").val(localStorage.getItem("hr13"));
-    // $("#hr14 .description").val(localStorage.getItem("hr14"));
-    // $("#hr15 .description").val(localStorage.getItem("hr15"));
-    // $("#hr16 .description").val(localStorage.getItem("hr16"));
-    // $("#hr17 .description").val(localStorage.getItem("hr17"));
+    // Save to Local Storage 
+    $("#hr9 .description").val(localStorage.getItem("hr9"));
+    $("#hr10 .description").val(localStorage.getItem("hr10"));
+    $("#hr11 .description").val(localStorage.getItem("hr11"));
+    $("#hr12 .description").val(localStorage.getItem("hr12"));
+    $("#hr13 .description").val(localStorage.getItem("hr13"));
+    $("#hr14 .description").val(localStorage.getItem("hr14"));
+    $("#hr15 .description").val(localStorage.getItem("hr15"));
+    $("#hr16 .description").val(localStorage.getItem("hr16"));
+    $("#hr17 .description").val(localStorage.getItem("hr17"));
 
     // Function to get time
     function timeTrack() {
