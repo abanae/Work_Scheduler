7f35fe8ede4f73cfd8f2471f4673ce494b0aeb6b
Ana Ea <aabad0464@gmail.com>
2021-05-20 21:48:43 -0600

    added for loops for time actions

--- assets/script.js
+++ assets/script.js
@@ -33,22 +33,22 @@ $().ready(function(){
             let blockHour = parseInt($(this).attr("id").split("hour")[1]);
             console.log(blockHour, currentTime)
             
-            // if (blockHour < time) {
-            //     $(this).removeClass("future");
-            //     $(this).removeClass("present");
-            //     $(this).addClass("past");
-            // }
-            // else if (blockHour === time) {
-            //     $(this).removeClass("past");
-            //     $(this).removeClass("future");
-            //     $(this).addClass("present");
-            // }
-            // else {
-            //     $(this).removeClass("present");
-            //     $(this).removeClass("past");
-            //     $(this).addClass("future");
+            if (blockHour < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-            // }
+            }
         })
     }
 
